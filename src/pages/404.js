b2f964622c0907744bb27717f851355e1cb54b0d
norayda <47399096+norayda@users.noreply.github.com>
2021-03-12 15:49:28 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ const NotFoundPage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
       <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>"Vous avez atteint un chemin qui n'existe pas... quelle tristess"</p>
     </Layout>
   )
 }
